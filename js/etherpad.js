93079d13e7657798854dd52d636a6a9358624a2f
John McLear <john@mclear.co.uk>
2011-08-13 23:11:14 +0100

    Added monospace stuff to index.html and began adding code to support getting a pads content

--- js/etherpad.js
+++ js/etherpad.js
@@ -1,9 +1,8 @@
 (function( $ ){
 
   $.fn.pad = function( options ) {
-
     var settings = {
-      'host'		 : 'http://beta.etherpad.org',
+      'host'		 : 'http://10.0.0.55:9001',
       'baseUrl'		 : '/p/',
       'showControls'     : false,
       'showChat'	 : false,
@@ -12,11 +11,24 @@
       'useMonospaceFont' : false
     };
 
-    if ( options ) { 
-      $.extend( settings, options );
+    if ( options != 'getContents' )
+    {
+      if ( options ) 
+      { 
+        $.extend( settings, options );
+      }
+      var iFrameLink = '<iframe src="'+settings.host+settings.baseUrl+settings.padId+'?showControls='+settings.showControls+'&showChat='+settings.showChat+'&showLineNumbers='+settings.showLineNumbers+'&useMonospaceFont='settings.useMonospaceFont+'"></iframe>';
+      console.log(iFrameLink);
+      this.html(iFrameLink);
     }
-    var iFrameLink = '<iframe src="'+settings.host+settings.baseUrl+settings.padId+'?showControls='+settings.showControls+'&showChat='+settings.showChat+'&showLineNumbers='+settings.showLineNumbers+'&useMonospaceFont='settings.useMonospaceFont+'"></iframe>';
-    console.log(iFrameLink);
-    this.html(iFrameLink);
+
+    if (options == 'getContents')
+    {
+      var url = $(this).get(0).location.href
+
+      // get the contents of this.
+      // get the link of the iframe using host + pathanme
+
+    }
   };
 })( jQuery );
