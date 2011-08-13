8aa194181f3d6200a617534c85eb513a32e5cd4a
John McLear <john@mclear.co.uk>
2011-08-13 22:11:12 +0100

    Add support for monospace fonts

--- js/etherpad.js
+++ js/etherpad.js
@@ -8,13 +8,14 @@
       'showControls'     : false,
       'showChat'	 : false,
       'showLineNumbers'  : false,
-      'userName'	 : 'unnamed'
+      'userName'	 : 'unnamed',
+      'useMonospaceFont' : false
     };
 
     if ( options ) { 
       $.extend( settings, options );
     }
-    var iFrameLink = '<iframe src="'+settings.host+settings.baseUrl+settings.padId+'?showControls='+settings.showControls+'&showChat='+settings.showChat+'&showLineNumbers='+settings.showLineNumbers+'"></iframe>';
+    var iFrameLink = '<iframe src="'+settings.host+settings.baseUrl+settings.padId+'?showControls='+settings.showControls+'&showChat='+settings.showChat+'&showLineNumbers='+settings.showLineNumbers+'&useMonospaceFont='settings.useMonospaceFont+'"></iframe>';
     console.log(iFrameLink);
     this.html(iFrameLink);
   };
